5a0038e36cc5cb1d00be1344732ef7c0c6bb5521
Emma A. A. Rünzel <emma@ruenzel.de>
2024-02-27 18:36:02 +0100

    Update Seeds & Views f/ Geo Lecture

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.destroy_all
+puts "Cleaned Database 🧼"
+sleep 1
+
+Flat.create(address: "Altenbraker Straße 26", name: "Emma HQ")
+Flat.create(address: "16 Villa Gaudelet, Paris", name: "Le Wagon Paris")
+Flat.create(address: "C. del Dr. Esquerdo 70", name: "Le Wagon Madrid")
+Flat.create(address: "Kochstraße 26", name: "Le Wagon Berlin")
+Flat.create(address: "380 Bd Brahim Roudani", name: "Le Wagon Casablanca")
+
+puts "#{Flat.count} flats built 🏠 🏠 🏠"
